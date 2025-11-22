520e8ba329d7a0609ee1df98d03745e1f172ec93
yad20072003-dev <yad20072003@gmail.com>
2025-11-22 06:23:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -96,5 +96,6 @@ async def main():
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 
-if name == "__main__":
+if __name__ == "__main__":
     asyncio.run(main())
+
